28e7733aae075f5f04e94986da3f1effc6976075
NateNgu <189771634+NateNgu@users.noreply.github.com>
2024-12-13 17:08:32 -0600

    feat: add button and put for loop inside function * issues: for loop isnt looping properly in function

--- javascript.js
+++ javascript.js
@@ -2,16 +2,24 @@ const container = document.getElementById("container");
 
 // Creates 16x6 grid
 
-for (rows = 0; rows < 16; rows++) {
-  const row = document.createElement("div"); // Create a row
-  row.classList.add("row")
-  container.appendChild(row);
-  for (column = 0; column < 16; column++) { // Add 16 squares into the row
-    const square = document.createElement("div");
-    square.classList.add("square");
-    row.appendChild(square);
+setGrid(16, 16);
+
+function setGrid(rows, columns) {
+  for (i = 0; i < rows; i++) {
+    const row = document.createElement("div"); // Create a row
+    row.classList.add("row");
+    console.log(rows)
+    container.appendChild(row);
+    for (k = 0; k < columns; k++) {
+      // Add 16 squares into the row
+      const square = document.createElement("div");
+      square.classList.add("square");
+      row.appendChild(square);
+    }
   }
 }
+
+sizeButton = document.getElementById("sizeButton");
 
 // Create hover effect
 
