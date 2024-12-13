7d1f900dea8bd3141e4cc9fcabbb79d7faf197e0
NateNgu <189771634+NateNgu@users.noreply.github.com>
2024-12-13 16:47:25 -0600

    feat: modify grid to use nested for loop instead * grid can now specify amount of rows and columns

--- javascript.js
+++ javascript.js
@@ -2,10 +2,15 @@ const container = document.getElementById("container");
 
 // Creates 16x6 grid
 
-for (i = 0; i < 256; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
-  container.appendChild(square);
+for (rows = 0; rows < 16; rows++) {
+  const row = document.createElement("div"); // Create a row
+  row.classList.add("row")
+  container.appendChild(row);
+  for (column = 0; column < 16; column++) { // Add 16 squares into the row
+    const square = document.createElement("div");
+    square.classList.add("square");
+    row.appendChild(square);
+  }
 }
 
 // Create hover effect
